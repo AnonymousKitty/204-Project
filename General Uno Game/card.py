a2f7f9e3ced1ad5d779fed27b90851c070cc1b9f
AnonymousKitty <111555926+AnonymousKitty@users.noreply.github.com>
2022-10-17 01:40:08 -0400

    Update card.py

--- General Uno Game/card.py
+++ General Uno Game/card.py
@@ -3,38 +3,47 @@ Card Class
 '''
 
 class Card:
-    cardnumber = -1
-    cardcolor = -1
-    cardtype = "None"
+    card_number = -1
+    card_color = -1
+    card_type = "None"
 
     def __init__(self, num, color, type):
-        self.cardnumber = num
-        self.cardcolor = color
-        self.cardtype = type
+        self.card_number = num
+        self.card_color = color
+        self.card_type = type
 
     def color_string(self):
-        if self.cardcolor == 0:
-            return "RED"
-        elif self.cardcolor == 1:
-            return "BLUE"
-        elif self.cardcolor == 2:
-            return "GREEN"
-        elif self.cardcolor == 3:
-            return "YELLOW"
+        if self.card_color == 0:
+            return "RED "
+        elif self.card_color == 1:
+            return "BLUE "
+        elif self.card_color == 2:
+            return "GREEN "
+        elif self.card_color == 3:
+            return "YELLOW "
         else:
             return ""
 
     def type_string(self):
-        if self.cardtype != "None":
-            if self.cardtype == "wild":
-                if self.cardnumber == 4:
-                    return "pick up 4 wild"
-            return self.cardtype
+        if self.card_type != "None":
+            if self.card_type == "wild":
+                if self.card_number == 4:
+                    return "pick up 4 wild "
+            return (self.card_type+ " ")
         else:
-            return str(self.cardnumber)
+            return ("" + str(self.card_number)+ " ")
 
-    def get_card_string(self):
-        return ""+ self.color_string()+ " "+ self.type_string()+ " card"
+    def get_string(self):
+        return ""+ self.color_string()+ self.type_string()+ "card"
+
+    def get_type(self):
+        return self.card_type
+
+    def get_number(self):
+        return self.card_number
+
+    def get_color(self):
+        return self.card_color
 
 # A card is either a wild card (wild) or has a color designated to it
 
