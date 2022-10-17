2391284364a6e1deab7d47953c841a3c75fd86f9
AnonymousKitty <111555926+AnonymousKitty@users.noreply.github.com>
2022-10-17 01:40:49 -0400

    Update player.py

--- General Uno Game/player.py
+++ General Uno Game/player.py
@@ -8,7 +8,34 @@ Player class
 
 class Player:
     cards = []
-    card_number = 0
-    def __init__(self, cards):
+    num_of_cards = 0
+    name = ""
+    def __init__(self, cards, name):
         self.cards = cards
-        self.card_number = len(cards)
+        self.num_of_cards = len(cards)
+        self.name = name
+
+    def get_card(self, card_num):
+        return self.cards[card_num]
+
+    def get_name (self):
+        return self.name
+
+    def get_num_cards(self):
+        return self.num_of_cards
+
+    def add_card(self, new_card):
+        self.cards.append(new_card)
+        self.num_of_cards += 1
+
+    def use_card(self, used_card):
+        if used_card == 0:
+            self.cards = self.cards[1:]
+        else:
+            temp_cards_1 = self.cards[0:used_card]
+            if used_card < self.num_of_cards:
+                temp_cards_2 = self.cards[used_card + 1:]
+                self.cards = temp_cards_1 + temp_cards_2
+            else:
+                self.cards = temp_cards_1
+        self.num_of_cards -= 1
