bc13f8d708b4b98bf639b9f1d7566d0e84d50ebf
AnonymousKitty <111555926+AnonymousKitty@users.noreply.github.com>
2022-11-30 15:59:10 -0500

    Update run.py

--- General Uno Game/run.py
+++ General Uno Game/run.py
@@ -19,15 +19,20 @@ DIRECTION = ['Left', 'Front', 'Right']
 # Propositions for the game
 PROPOSITIONS = []
 
+
 class Unique(object):
-  def __hash__(self):
-    return hash(str(self))
-  def __eq__(self, other):
-    return hash(self) == hash(other)
-  def __repr__(self):
-    return str(self)
-  def __str__(self):
-    assert False, "You need to define the __str__ function on a proposition class"
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        assert False, "You need to define the __str__ function on a proposition class"
+
 
 @proposition(E)
 class NumberColorType(Unique):
@@ -38,7 +43,7 @@ class NumberColorType(Unique):
         self.type = type
         self.position = position
 
-    def __repr__(self):
+    def __str__(self):
         return f"card@{self.position}=[{self.number},{self.color},{self.type}]"
 
 
@@ -49,7 +54,6 @@ for number in NUMBERS:
             for position in POSITIONS:
                 PROPOSITIONS.append(NumberColorType(number, color, type, position))
 
-
 @proposition(E)
 class NumOfCards(Unique):
     # for whether or not players have 3 cards or not
@@ -57,21 +61,23 @@ class NumOfCards(Unique):
         self.player = player
         self.hasMoreThanThree = bool
 
-    def __repr__(self):
+    def __str__(self):
         return f"player@{self.player}={self.hasMoreThanThree}"
 
 
 # create all possibilities other players have 3 cards or not
-for player in DIRECTION:
-    for bool in NUMOFCARDS:
-        PROPOSITIONS.append(NumOfCards(player, bool))
+#for player in DIRECTION:
+#    for bool in NUMOFCARDS:
+#        PROPOSITIONS.append(NumOfCards(player, bool))
 
-for positions in POSITIONS:
-  constraint.add_exactly_one(E, [NumberColorType(number, color, type)])
+for position in POSITIONS:
+    for number in NUMBERS:
+        for type in TYPES:
+            constraint.add_exactly_one(E, [NumberColorType(number, color, type, position) for color in COLORS])
+
 
 # other options include: at most one, exactly one, at most k, and implies all.
 # For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
 # Build an example full theory for your setting and return it.
 #
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
@@ -80,57 +86,69 @@ for positions in POSITIONS:
 # CONSTRAINTS
 
 # A card need to be a valid card if: 
-# 1) the card is a "regular" card with a number and aacolor
+# 1) the card is a "regular" card with a number and a color
 # 2) the card is a "wild" or "wild pick up" card with neither number nor color
 # 3) the card is a "reverse" or "skip" card with a color but no number
 # Add custom constraints by creating formulas with the variables you created.
 
+# for each type, there exists a constraint on wether there will be a number and/or color or not
+
+
 for position in POSITIONS:
-  for number in NUMBERS:
-      for type in TYPES:
-        if type == 'regular':
-          if number != None:
-            constraint.add_exactly_one(E, [NumberColorType(number, color, type, position) for color in COLORS) 
-        else if type == 'wild' or type == 'wild pick up':
-          if number == None:
-            constraint.add_exactly_one(E, [NumberColorType(number, None, type, position)]) 
-        else:
-          if number == None:
-            constraint.add_exactly_one(E, [NumberColorType(number, color, type, position)] for color in COLORS) 
-              
+    for number in NUMBERS:
+        for type in TYPES:
+            if type == 'regular':
+                if number != None:
+                    # adds exactly one card if number is not none and it is a regular type
+                    print("a")
+                    constraint.add_exactly_one(E, [NumberColorType(number, color, type, position) for color in COLORS])
+                    # otherwise if card type is wild or wild pick up, then:
+            elif (type == 'wild' or type == 'wild pick up'):
+                if number == None:
+                    print("b")
+                    # adds exactly one card if number is not none and it is a regular type
+                    constraint.add_exactly_one(E, [NumberColorType(number, None, type, position)])
+            else:
+                if number == None:
+                    print("c")
+                    constraint.add_exactly_one(E, [NumberColorType(number, color, type, position) for color in COLORS])
 
 
-            
 def example_theory():
-    # If the palyer has a card that is the same color as the top card, it can be played
+    # A card can be played if:
+    # 1) has the same color with the top card
+    # 2) has the same number with the top card
+    # 3) wild pick up card?
 
     pass
-    #E.add_constraint((a | b) & ~x)
+    # E.add_constraint((a | b) & ~x)
     # Implication
-    #E.add_constraint(y >> z)
+    # E.add_constraint(y >> z)
     # Negate a formula
-    #E.add_constraint(~(x & y))
+    # E.add_constraint(~(x & y))
     # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
     # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    #constraint.add_exactly_one(E, a, b, c)
+    # constraint.add_exactly_one(E, a, b, c)
 
-    #return E
+    # return E
 
-
-if __name__ == "__main__":
-
-    T = example_theory()
-    # Don't compile until you're finished adding all your constraints!
-    T = T.compile()
+T = E.compile()
+print("# Solutions: %d" % count_solutions(T) )
+exit(0)
+#if __name__ == "__main__":
+#
+ #   T = example_theory()
+  #  # Don't compile until you're finished adding all your constraints!
+   # T = T.compile()
     # After compilation (and only after), you can check some of the properties
     # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
+    #print("\nSatisfiable: %s" % T.satisfiable())
+    #print("# Solutions: %d" % count_solutions(T))
+   # print("   Solution: %s" % T.solve())
 
-    print("\nVariable likelihoods:")
-    for v, vn in zip([a, b, c, x, y, z], 'abcxyz'):
+    #print("\nVariable likelihoods:")
+    #for v, vn in zip([a, b, c, x, y, z], 'abcxyz'):
         # Ensure that you only send these functions NNF formulas
         # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
+    #    print(" %s: %.2f" % (vn, likelihood(T, v)))
+    #print()
