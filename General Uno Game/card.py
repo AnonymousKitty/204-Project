b75c853f520fdde714b6dd60bd69029495119a8b
AnonymousKitty <111555926+AnonymousKitty@users.noreply.github.com>
2022-12-22 19:12:27 -0500

    Update card.py
    added features for playable powerups

--- General Uno Game/card.py
+++ General Uno Game/card.py
@@ -6,6 +6,7 @@ class Card:
     card_number = -1
     card_color = -1
     card_type = "None"
+    played_out = False
 
     def __init__(self, num, color, type):
         self.card_number = num
@@ -26,9 +27,8 @@ class Card:
 
     def type_string(self):
         if self.card_type != "None":
-            if self.card_type == "wild":
-                if self.card_number == 4:
-                    return "pick up 4 wild "
+            if self.card_type == "wild pickup":
+                return "pick up 4 wild "
             return (self.card_type+ " ")
         else:
             return ("" + str(self.card_number)+ " ")
@@ -45,6 +45,17 @@ class Card:
     def get_color(self):
         return self.card_color
 
+    def change_color(self, color):
+        if self.get_type() == "wild" or self.get_type() == "wild pickup":
+            self.card_color = color
+        else:
+            print('error')
+
+    def check_played(self):
+        return self.played_out
+
+    def change_played(self):
+        self.played_out = True
 # A card is either a wild card (wild) or has a color designated to it
 
 # A card is either a power card (power) or has a number designated to it
